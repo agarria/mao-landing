fe1e5cb74d2921321f591816384b7afe5c86f373
agarria <agarria@icloud.com>
2020-07-22 18:04:02 -0600

    Update

---
+++ mao.js
@@ -0,0 +1,6 @@
+function menu() {
+    var menu = document.getElementById("menu");
+    var icon = document.getElementById("icon");
+    menu.classList.toggle("open-menu");
+	icon.classList.toggle("open-icon");
+}
